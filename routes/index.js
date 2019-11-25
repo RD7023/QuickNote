0aa983e0ca912caa7f8c583bbe83078ef0ee6f1a
RD7023 <r2d20@ukr.net>
2019-11-25 19:30:01 +0200

    Udated Sign Up, added login and log out

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Form Validation', success: req.session.success, errors:req.session.errors});
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors:req.session.errors, username:req.session.username, email:req.session.userEmail});
   req.session.errors=null;
-  req.session.success=null;
 });
 
-router.post('/submit',function(req,res,next){
+router.post('/submitSignUp',function(req,res,next){
   //Check validity
-  req.check('email','Invalid email address').isEmail();
-  req.check('password','Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword)
-
-
+  req.check('emailSignUp','Invalid email address').isEmail();
+  req.check('passwordSignUp','Password is invalid').isLength({min: 4}).equals(req.body.confirmPasswordSignUp)
 
   var errors = req.validationErrors();
   if (errors) {
@@ -23,10 +20,31 @@ router.post('/submit',function(req,res,next){
     req.session.success = false;
   }
   else {
-    req.session.success = true;
     db.createUser(req, res);
   }
-  res.redirect('/')
 })
 
+router.post('/submitLogin',function (req,res,next) {
+  //Check validity
+  req.check('emailLogin','Invalid email address').isEmail();
+  req.check('passwordLogin','Password is invalid').isLength({min: 4});
+
+  var errors = req.validationErrors();
+  console.log(req.body.emailLogin);
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+    res.redirect('/')
+  }
+  else {
+    db.validateUserByLoginEndPassword(req, res);
+  }
+})
+
+router.post('/logout',function (req,res,next) {
+  req.session.success = false;
+  req.session.username=null;
+  req.session.userEmail=null;
+  res.redirect('/')
+})
 module.exports = router;
--- routes/queries.js
+++ routes/queries.js
@@ -10,15 +10,68 @@ const pool = new Pool({
 })
 
 const createUser = (request, response) => {
-  const { nickname,password,email } = request.body
-  pool.query('INSERT INTO users (nickname,password,email) VALUES ($1, $2, $3)', [nickname, passwordHash.generate(password) ,email], (error, results) => {
+  const { nickname,passwordSignUp,emailSignUp } = request.body;
+  pool.query('SELECT * FROM users WHERE nickname=$1 OR email=$2',[nickname,emailSignUp],(error,results) => {
+
+    if (!results.rows[0]) {
+      pool.query('INSERT INTO users (nickname,password,email) VALUES ($1, $2, $3)', [nickname, passwordHash.generate(passwordSignUp) ,emailSignUp], (error2, results2) => {
+        if (error2) {
+          throw error2;
+        }
+        else {
+          request.session.success =true;
+          request.session.username=nickname;
+          request.session.userEmail=emailSignUp;
+          response.redirect('/');
+        }
+      })
+    }
+    else{
+      request.session.success = false;
+      var errors = [{msg:'There already exist user with such email or nickname'}]
+      request.session.errors = errors;
+      response.redirect('/');
+    }
+  })
+
+
+}
+const validateUserByLoginEndPassword = (request,response) => {
+  const {passwordLogin,emailLogin} = request.body;
+
+  pool.query('SELECT * FROM users WHERE email=$1',[emailLogin],(error,results) => {
     if (error) {
       throw error;
     }
-    response.status(201);
+    else {
+      if (results.rows[0]) {
+        if (passwordHash.verify(passwordLogin,results.rows[0].password)) {
+          request.session.success =true;
+          request.session.username=results.rows[0].nickname;
+          request.session.userEmail=results.rows[0].email;
+          response.redirect('/');
+        }
+        else {
+          request.session.success =false;
+          var errors = [{msg:'Incorrect password'}]
+          request.session.errors = errors;
+          console.log(request.session.errors[0].msg);
+          response.redirect('/');
+        }
+      }
+      else {
+        request.session.success =false;
+        var errors = [{msg:'There is no user with email: ' + emailLogin}]
+        request.session.errors = errors;
+        response.redirect('/');
+      }
+    }
+
+
   })
 }
 
 module.exports = {
   createUser,
+  validateUserByLoginEndPassword
 }
